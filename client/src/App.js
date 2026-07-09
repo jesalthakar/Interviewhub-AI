59466fd6b270695d2896672745c5275562af301f
unknown <jesalthakar30@gmail.com>
2026-07-09 22:56:40 +0530

    first commit-folder structure done

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to InterviewHub AI</h1>
+        <p>MERN Stack Application</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
